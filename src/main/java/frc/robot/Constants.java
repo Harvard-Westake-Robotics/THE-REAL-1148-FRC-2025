43a163294a1244344b8f79815f0324cc31be72a5
OG Wolverine <113261036+HarvardWestlake@users.noreply.github.com>
2025-02-08 14:42:45 -0800

    Added Files

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,49 @@
+// Copyright 2021-2025 FRC 6328
+// http://github.com/Mechanical-Advantage
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// version 3 as published by the Free Software Foundation or
+// available in the root directory of this project.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.RobotBase;
+
+/**
+ * This class defines the runtime mode used by AdvantageKit. The mode is always "real" when running
+ * on a roboRIO. Change the value of "simMode" to switch between "sim" (physics sim) and "replay"
+ * (log replay from a file).
+ */
+public final class Constants {
+  public static final Mode simMode = Mode.SIM;
+  public static final Mode currentMode = RobotBase.isReal() ? Mode.REAL : simMode;
+
+  public static enum Mode {
+    /** Running on a real robot. */
+    REAL,
+
+    /** Running a physics simulator. */
+    SIM,
+
+    /** Replaying from a log file. */
+    REPLAY
+  }
+
+  public final class Intake {
+    public static final int motorId = 17;
+    public static final boolean motorInverted = false;
+    public static final double intakeVelocity = 100;
+    public static final double outtakeVelocity = -100;
+    public static double kP = 0.369162;
+    public static double kI = 0.0;
+    public static double kD = 0.0;
+    public static double kS = 0.1761;
+    public static double kV = 0.12875;
+  }
+}
